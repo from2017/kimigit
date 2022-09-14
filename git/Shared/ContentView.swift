84f5f473a37360b3fcb1359a6d4888922915f735
kimi <aptiv111@163.com>
2022-09-14 18:07:40 +0800

    first commit

---
+++ git/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 放风啊 on 2022/9/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ git/Shared/gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitApp.swift
+//  Shared
+//
+//  Created by 放风啊 on 2022/9/14.
+//
+
+import SwiftUI
+
+@main
+struct gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
